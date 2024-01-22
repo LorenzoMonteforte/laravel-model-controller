0df28f41e711dbbf0ddcbf2d35f24c93c8b76782
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-01-22 19:19:26 +0100

    Esercizio

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,3 +1,12 @@
 <main>
-    
+    @for ($i=0; $i<sizeof($movies); $i++)
+        <ul>
+            <li>ID: {{ $movies[$i]["id"] }}</li>
+            <li>Titolo: {{ $movies[$i]["title"] }}</li>
+            <li>Titolo originale: {{ $movies[$i]["original_title"] }}</li>
+            <li>Nazionalità: {{ $movies[$i]["nationality"] }}</li>
+            <li>Data: {{ $movies[$i]["date"] }}</li>
+            <li>Voto: {{ $movies[$i]["vote"] }}</li>
+        </ul>
+    @endfor
 </main>
